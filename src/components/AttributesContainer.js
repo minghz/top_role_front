f4627cc3e1685d53968d539f5953a275d5247a9d
Minghua Zhao <minghz42@gmail.com>
2019-11-06 05:12:00 -0500

    modify attributes from race selection

--- src/components/AttributesContainer.js
+++ src/components/AttributesContainer.js
@@ -6,12 +6,12 @@ class AttributesContainer extends Component {
   render() {
     return(
       <div className="attributes-container">
-        <Attribute name="Strength"     value={this.props.attributes.str} modifier={this.props.modifiers.str}/>
-        <Attribute name="Dexterity"    value={this.props.attributes.dex} modifier={this.props.modifiers.dex}/>
-        <Attribute name="Constitution" value={this.props.attributes.con} modifier={this.props.modifiers.con}/>
-        <Attribute name="Inteligence"  value={this.props.attributes.int} modifier={this.props.modifiers.int}/>
-        <Attribute name="Wisdom"       value={this.props.attributes.wis} modifier={this.props.modifiers.wis}/>
-        <Attribute name="Charisma"     value={this.props.attributes.cha} modifier={this.props.modifiers.cha}/>
+        <Attribute name="Strength"     value={this.props.attributes.str} racial={this.props.attributes_racial.str} modifier={this.props.modifiers.str}/>
+        <Attribute name="Dexterity"    value={this.props.attributes.dex} racial={this.props.attributes_racial.dex} modifier={this.props.modifiers.dex}/>
+        <Attribute name="Constitution" value={this.props.attributes.con} racial={this.props.attributes_racial.con} modifier={this.props.modifiers.con}/>
+        <Attribute name="Inteligence"  value={this.props.attributes.int} racial={this.props.attributes_racial.int} modifier={this.props.modifiers.int}/>
+        <Attribute name="Wisdom"       value={this.props.attributes.wis} racial={this.props.attributes_racial.wis} modifier={this.props.modifiers.wis}/>
+        <Attribute name="Charisma"     value={this.props.attributes.cha} racial={this.props.attributes_racial.cha} modifier={this.props.modifiers.cha}/>
       </div>
     )
   }
--- src/services/helpers.js
+++ src/services/helpers.js
@@ -16,7 +16,18 @@ export function proficiencyBonusFromLevel(level) {
 
 export function modifierFromAttribute(attribute) {
   return Math.floor((attribute - 10) / 2);
-};
+}
+
+export function modifiers(base, racial) {
+  return {
+    str: modifierFromAttribute(base.str + racial.str),
+    dex: modifierFromAttribute(base.dex + racial.dex),
+    con: modifierFromAttribute(base.con + racial.con),
+    int: modifierFromAttribute(base.int + racial.int),
+    wis: modifierFromAttribute(base.wis + racial.wis),
+    cha: modifierFromAttribute(base.cha + racial.cha)
+  }
+}
 
 export function listNames(array) {
   return Array.from(array, race => race["name"])
@@ -25,5 +36,12 @@ export function listNames(array) {
 export function racialAttributes(raceName) {
   var obj = RacesData.find(({ name }) => name === raceName)
 
-  return obj.attributes;
+  return {
+    str: obj.attributes.str || 0,
+    dex: obj.attributes.dex || 0,
+    con: obj.attributes.con || 0,
+    int: obj.attributes.int || 0,
+    wis: obj.attributes.wis || 0,
+    cha: obj.attributes.cha || 0
+  }
 }
