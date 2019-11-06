574f476365bf0d86eabc47ff4015d86cb6a4d045
Minghua Zhao <minghz42@gmail.com>
2019-11-06 04:44:26 -0500

    Allow change to a different race

--- src/components/Sheet.js
+++ src/components/Sheet.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import '../css/Sheet.css';
 import * as helpers from '../services/helpers';
 
-import HeaderContainer from './HeaderContainer';
+import '../css/HeaderContainer.css';
+import CharName from './header_container/CharName';
+import CharRace from './header_container/CharRace';
+import CharBackground from './header_container/CharBackground';
+
 import LevelsContainer from './LevelsContainer';
 import AttributesContainer from './AttributesContainer';
 import SkillsContainer from './SkillsContainer';
@@ -14,13 +18,24 @@ import ItemsContainer from './ItemsContainer';
 
 import AttributesData from '../data/attributes.json';
 import ProficienciesData from '../data/proficiencies.json';
+import RacesData from '../data/races.json';
 
 class Sheet extends Component {
   constructor(props){
     super(props);
     this.state = {
+      char_name: "Tracx Lury",
+      race: "Dragonborn",
       level: 5,
       proficiency_bonus: helpers.proficiencyBonusFromLevel(5),
+      attributes: {
+        str: AttributesData.str,
+        dex: AttributesData.dex,
+        con: AttributesData.con,
+        int: AttributesData.int,
+        wis: AttributesData.wis,
+        cha: AttributesData.cha,
+      },
       attributes_modifiers: {
         str: helpers.modifierFromAttribute(AttributesData.str),
         dex: helpers.modifierFromAttribute(AttributesData.dex),
@@ -28,16 +43,34 @@ class Sheet extends Component {
         int: helpers.modifierFromAttribute(AttributesData.int),
         wis: helpers.modifierFromAttribute(AttributesData.wis),
         cha: helpers.modifierFromAttribute(AttributesData.cha),
-      }
+      },
+      attributes_racial: helpers.racialAttributes("Dragonborn")
     }
+
+    this.handleRaceChange = this.handleRaceChange.bind(this);
   };
+
+  handleRaceChange(raceName) {
+    this.setState({
+      attributes_racial: helpers.racialAttributes(raceName),
+      race: raceName,
+    });
+  }
+
   render() {
     return (
       <div className="sheet">
-        <HeaderContainer />
+        <div className="header-container">
+          <CharName value={this.state.char_name} />
+          <CharRace
+            value={this.state.race}
+            races={helpers.listNames(RacesData)}
+            onRaceChange={this.handleRaceChange} />
+          <CharBackground value="Far Traveler"/>
+        </div>
         <LevelsContainer value={this.state.level}/>
         <AttributesContainer
-          attributes={AttributesData}
+          attributes={this.state.attributes}
           modifiers={this.state.attributes_modifiers}/>
         <SkillsContainer
           proficiencies={ProficienciesData}
