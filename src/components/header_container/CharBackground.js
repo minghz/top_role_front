2b47a443ad5c906954d0bb209411e52c4ee784f5
Minghua Zhao <minghz42@gmail.com>
2019-12-14 18:15:04 -0500

    feat: Preliminary character background selection
    * selection of PHB backgrounds
    * updates descriptions in the char section
    * future work: update proficiencies, equipment, features, etc

--- src/components/header_container/CharBackground.js
+++ src/components/header_container/CharBackground.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
 class CharBackground extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value,
+      backgrounds: props.backgrounds
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.currentTarget.value});
+    this.props.onBackgroundChange(event.currentTarget.value);
+  }
+
   render(){
     return(
       <div className="char-background">
-        {this.props.value}
+        <strong>Background</strong>
+        <select value={this.state.value} onChange={this.handleChange} >
+          { this.state.backgrounds.map((value, index) => {
+            return <option key={index} value={value}>{value}</option>
+          })}
+        </select>
       </div>
     );
   }
