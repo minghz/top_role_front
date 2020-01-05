4e00210e05330d2bb0c4464f4a568be7522f79a2
Minghua Zhao <minghz42@gmail.com>
2020-01-04 22:15:01 -0500

    Updates

---
+++ precache-manifest.8a61b79a73057f439440f7f42bdc2ca0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb363200dbcbd1a85394ef7d418790c",
+    "url": "/top_role_front/index.html"
+  },
+  {
+    "revision": "1dded3ebc1fc74cade84",
+    "url": "/top_role_front/static/css/main.9fd560dd.chunk.css"
+  },
+  {
+    "revision": "077dc4ceaf4c20dc0613",
+    "url": "/top_role_front/static/js/2.d2350d2d.chunk.js"
+  },
+  {
+    "revision": "1dded3ebc1fc74cade84",
+    "url": "/top_role_front/static/js/main.6d114583.chunk.js"
+  },
+  {
+    "revision": "482fa224de264a28fa2b",
+    "url": "/top_role_front/static/js/runtime-main.b13f4fff.js"
+  }
+]);
